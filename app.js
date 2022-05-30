d67f2c3be13f166d9f3fddeda39757d6afbaa76a
majidalykhan <majidalykhan@gmail.com>
2022-05-30 17:27:36 +0500

    Setup routes and contollers - get all tasks

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+const tasks = require("./routes/tasks");
+
 const port = 3000;
+
+//Middleware
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
+
+app.use("/api/v1/tasks", tasks);
 
 app.listen(port, console.log(`Server is listening on Port: ${port}`));
---
+++ controllers/tasks.js
@@ -0,0 +1,5 @@
+const getAllTasks = (req, res) => {
+  res.send("All tasks from file");
+};
+
+module.exports = { getAllTasks };
