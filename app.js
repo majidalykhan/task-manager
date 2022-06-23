25f91324da66aa8f84a46591ae1498df470d007e
majidalykhan <majidalykhan@gmail.com>
2022-06-23 15:41:25 +0500

    Successful connection to mongoDB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("./db/connect");
 const express = require("express");
 const app = express();
 
@@ -15,3 +16,4 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 app.listen(port, console.log(`Server is listening on Port: ${port}`));
+
