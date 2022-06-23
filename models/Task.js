46f7d0f4366581a2460210d9d899ba934b9a45b5
majidalykhan <majidalykhan@gmail.com>
2022-06-23 17:23:16 +0500

    Added validations for properties

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Name cannot be empty"],
+    trim: true,
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
